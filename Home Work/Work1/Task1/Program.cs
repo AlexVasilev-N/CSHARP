56f4d9b3162a3dffc7c4ae3113aed58ba0101c5e
AlexVasilev-N <alexvasilievspb@gmail.com>
2023-01-08 03:12:44 +0300

    hi

---
+++ Home Work/Work1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+Console.WriteLine("Введите число:");
+
+string numberStr = Console.ReadLine() ?? "";// fsdfdsdsf, 1223423 "fsdfdsdsf", "1223423"
+int number = int.Parse(numberStr);// "323" -> 323
+int result = number * number;
+
+Console.WriteLine($"квадрат числа {number} = {result}");
+Console.WriteLine("квадрат числа {number} = {result}");
+
+
