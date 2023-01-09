958fb8d51dcb1873eaaf58d2599fd1fbca90e4ee
AlexVasilev-N <alexvasilievspb@gmail.com>
2023-01-09 19:03:43 +0300

    next commit

--- Home Work/Work1/Task1/Program.cs
+++ Home Work/Work1/Task1/Program.cs
@@ -1,12 +1,20 @@
-﻿
+﻿/*
+Задача 2: Напишите программу, которая на вход принимает два 
+числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите число:");
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
 
-string numberStr = Console.ReadLine() ?? "";// fsdfdsdsf, 1223423 "fsdfdsdsf", "1223423"
-int number = int.Parse(numberStr);// "323" -> 323
-int result = number * number;
 
-Console.WriteLine($"квадрат числа {number} = {result}");
-Console.WriteLine("квадрат числа {number} = {result}");
+int num = 5 ;
+int b = 7 ;
+int sum = num + b ;
+
+
+
+Console.WriteLine ();
+
 
 
