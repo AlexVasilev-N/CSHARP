0a83699a7304d8eb268730b1c96c00aecd6f5b73
AlexVasilev-N <alexvasilievspb@gmail.com>
2023-01-17 14:01:05 +0300

    Two Lection

---
+++ Lecture/Lecture_2/Exampel_001/Program.cs
@@ -0,0 +1,18 @@
+﻿// Поиск индекса по значению
+
+
+int [] array = {1,3,5,6,7,8,9,4,55,33,};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array [index] ==find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
