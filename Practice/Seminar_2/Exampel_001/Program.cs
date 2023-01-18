6a4aed77782db745f63bac0a08d28703f57be110
AlexVasilev-N <alexvasilievspb@gmail.com>
2023-01-18 17:54:47 +0300

    test commit

---
+++ Practice/Seminar_2/Exampel_001/Program.cs
@@ -0,0 +1,62 @@
+﻿/*
+Напишите программу, которая выводит случайное число из отрезка [10, 99] и п
+оказывает наибольшую цифру числа.
+
+Например:
+78 -> 8
+12-> 2
+85 -> 8
+*/
+
+
+int GetConsoleNumber (string message)
+{
+    int result = 0;
+
+    while (true)
+    {
+        Console.WriteLine (message);
+
+        if (int.TryParse (Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine( "Ввели не число: ");
+        }
+
+    }
+    return result;
+}
+
+
+int GetRandomNumber (int leftBoard, int rightBoard)
+{
+    Random rnd = new Random();
+
+    int result = rnd.Next(leftBoard, rightBoard + 1);
+
+    return result;
+}
+
+int leftRange = GetConsoleNumber ("Введите число 1");
+int rightRane = GetConsoleNumber ("введите число 2");
+int number = GetRandomNumber(leftRange, rightRane);
+
+Console.WriteLine ($"Рандомное число: {number} ");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+
+else if (secondDigit > firstDigit)
+{
+    Console.WriteLine (secondDigit);
+}
+else
+    Console.WriteLine ("числа равны");
---
+++ Practice/Seminar_2/Exampel_002/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Напишите программу, которая выводит случайное т
+рехзначное число и удаляет вторую цифру этого числа.
+
+456 -> 46
+782 -> 72
+918 -> 98
+
+*/
+
+//генерирует случайно число в границах [leftBoard, rightBoard]
+
+int GetRandomNumber(int leftBoard, int rightBoard)
+{
+Random rnd = new Random();
+
+int result = rnd.Next(leftBoard, rightBoard + 1); //[leftBoard, rightBoard)
+
+return result;
+}
+
+
+int leftRange = GetNumberFromConsole("Введите левую границу диапазона рандомного значения:");
+int rightRange = GetNumberFromConsole("Введите правую границу диапазона рандомного значения:");
+int number = GetRandomNumber(leftRange, rightRange);
+
+Console.WriteLine($"Рандомное число: {number}");
+
+
+
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+
+int result = firstDigit * 10 + thirdDigit;
+
+Console.WriteLine(result);
